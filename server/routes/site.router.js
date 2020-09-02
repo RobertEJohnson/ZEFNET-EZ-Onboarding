9baf362ee09148207401d30f2a14a12b61af1c85
Ace Fox <afox09@gmail.com>
2020-09-02 12:45:39 -0500

    get route

---
+++ server/routes/site.router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+/**
+ * GET sites by orgaization_id
+ */
+router.get('/:id', rejectUnauthenticated, (req, res) => {
+    const query = `SELECT * FROM "site" WHERE "organization_id" = $1`
+    const queryValue = [
+        req.params.id
+     ]
+    pool.query(query, queryValue)
+    .then((result) => {res.send(result.rows)
+    })  
+    .catch((error)=>{
+    res.sendStatus(500)
+    console.log(error);
+    })
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
