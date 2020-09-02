f24c8a6ce635efbce92ff8e444588d7e87eccc06
Ace Fox <afox09@gmail.com>
2020-09-02 14:14:53 -0500

    newDevicen

--- src/redux/reducers/newDevice.js
+++ src/redux/reducers/newDevice.js
@@ -1,9 +1,17 @@
 const newDeviceReducer = (state = {}, action) => {
     switch (action.type) {
       case 'SET_DEVICE_SITE':
-        return {site:action.payload};
-       case 'SET_BREAKER':
-        return {site:action.payload};
+        return {site: action.payload};
+      case 'SET_BREAKER':
+        return {breaker: action.payload};
+      case 'SET_SERIAL':
+        return {serial: action.payload};
+      case 'SET_TYPE':
+        return {type: action.payload};
+      case 'SET_NAME':
+        return {name: action.payload};
+      case 'SET_DATE':
+        return {date: action.payload};
       default:
         return state;
     }
