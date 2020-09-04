18b55ed9f8e5756557b3730ab6370b74b4b06c0e
Rob Wolfe <robertejohnson10@gmail.com>
2020-09-04 15:35:57 -0500

    removed Grid container parent

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -8,8 +8,6 @@ class WelcomeScreen extends Component {
   render() {
     let centerText = {textAlign: 'center', color: 'white', fontFamily: 'Crimson Text, Open Sans, sans-serif'}
     return (
-        <Grid container justify='center' alignContent='center' alignItems='center' 
-            style={{minHeight: '75vh', minWidth: '100vw', background: 'linear-gradient(360deg, #041E41, #004e92 70%)'}}>
             <Grid item xs={8} style={{ maxWidth: '1000px'}}  >
                     <h1 style={centerText}>Welcome to the ZEFNET EZ onboarding site!</h1>
                     <p style={centerText}>
@@ -24,8 +22,6 @@ class WelcomeScreen extends Component {
                         </Button>
                     </p>
             </Grid>
-        </Grid>
-      
     );
   }
 }
