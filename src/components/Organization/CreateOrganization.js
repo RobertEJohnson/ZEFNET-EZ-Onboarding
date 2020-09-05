824dabd10325370356bc5f44801e665fd5745c30
Rob Wolfe <robertejohnson10@gmail.com>
2020-09-05 01:51:29 -0500

    updated textfield coloring, removed a div class

--- src/components/Organization/CreateOrganization.js
+++ src/components/Organization/CreateOrganization.js
@@ -6,13 +6,11 @@ import PropTypes from 'prop-types'
 
 const styles = theme => ({ 
     input: {
+      color: 'white',
       border: `1px solid white`,
-      outline: `1px solid transparent`, // we use a transparent outline here so the component doesn't move when focused
-      focused: {
-        border: `1px solid green`,
-        outline: `1px solid yellow`, // this outline makes our "border" thicker without moving the component
-        background: 'white'
-      }},
+      backgroundColor: '#243353',
+      outline: `1px solid transparent`,// we use a transparent outline here so the component doesn't move when focused
+      },
       h1Reset: {
         textAlign: "center",
         color: "white",
@@ -25,12 +23,8 @@ const styles = theme => ({
         color: "white",
         fontFamily: "Crimson Text, Open Sans, sans-serif",
         padding: '0px',
-        margin: '0px 0px 10px 0px'
+        margin: '0px 0px 0px 0px'
       },
-      header: {
-        border: "solid #e3e3e3 2px",
-        maxWidth: "450px",
-      }
 })
 
 
@@ -67,47 +61,67 @@ class CreateOrganization extends Component {
 
     return (
         <Grid item xs={8} style={{ maxWidth: "1000px" }} align="center">
-          <div className={classes.header}>
+          <div>
             <h1 className={classes.h1Reset}>Organization Information</h1>
-            <h3 className={classes.h3Reset}>
+            <h2 className={classes.h3Reset}>
               This will help us associate the chargers with your organization.
-            </h3>
+            </h2>
           </div>
           <form style={{ minWidth: "400px", minHeight: "400px", margin: '0px', padding:'0px', background: "transparent" }}>
             <TextField
               required
-              color="secondary"
-              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', paddingTop: '25px', margin: '0px' }}
+              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', margin: '25px 0px 0px 0px'  }}
               label="Organization / Company Name"
               margin="normal"
               variant="outlined"
               value={this.state.organizationName}
               onChange={this.handleInputChangeFor("organizationName")}
+              InputProps={{
+                classes: {
+                  root: classes.input,
+                }
+              }}
+              InputLabelProps={{
+                style: { color: '#fff' }
+              }}
             />
             <TextField
-              color="secondary"
-              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', paddingTop: '15px', margin: '0px' }}
+              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', margin: '15px 0px 0px 0px' }}
               label="Phone Number"
               margin="normal"
               variant="outlined"
               type="number"
               value={this.state.primaryNumber}
               onChange={this.handleInputChangeFor("primaryNumber")}
+              InputProps={{
+                classes: {
+                  root: classes.input,
+                }
+              }}
+              InputLabelProps={{
+                style: { color: '#fff' },
+              }}
             />
             <TextField
-              color="secondary"
               required
-              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', paddingTop: '15px', margin: '0px' }}
+              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', margin: '15px 0px 0px 0px'}}
               label="Email"
               margin="normal"
               variant="outlined"
               value={this.state.email}
               onChange={this.handleInputChangeFor("email")}
+              InputProps={{
+                classes: {
+                  root: classes.input,
+                }
+              }}
+              InputLabelProps={{
+                style: { color: '#fff' },
+              }}
             />
             <TextField
-              color="secondary"
               required
-              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', paddingTop: '15px', margin: '0px' }}
+              style={{ minWidth: "380px", fontFamily: "Crimson Text", padding: '0px', margin: '15px 0px 0px 0px'}}
               label="Organization Address"
               variant="outlined"
               value={this.state.organizationAddress}
@@ -117,10 +131,13 @@ class CreateOrganization extends Component {
                   root: classes.input,
                 }
               }}
+              InputLabelProps={{
+                style: { color: '#fff' },
+              }}
             />
             <Button
               variant="contained"
-              style={{ marginTop: "20px", margin: '15px 0px 0px 0px' }}
+              style={{marginTop: "20px", margin: '15px 0px 0px 0px', color: '#006dcc', backgroundColor: 'white'}}
               color="default"
               onClick={this.handleAddOrg}
             >
