220ee4f8988f5abd3a730e631611c2fac664c09c
Rob Wolfe <robertejohnson10@gmail.com>
2020-09-07 09:55:06 -0500

    removed unused routes and components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,6 @@ import {
 import { connect } from "react-redux";
 
 import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
-import AboutPage from "../AboutPage/AboutPage";
-import InfoPage from "../InfoPage/InfoPage";
 import WelcomeScreen from "../WelcomeScreen/WelcomeScreen";
 import CreateOrganization from "../Organization/CreateOrganization";
 import ViewOrganization from "../Organization/ViewOrganization";
@@ -53,13 +51,6 @@ class App extends Component {
             <Switch>
               {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
               <Redirect exact from="/" to="/home" />
-              {/* Visiting localhost:3000/about will show the about page.
-              This is a route anyone can see, no login necessary */}
-              <Route exact path="/about" component={AboutPage} />
-              {/* For protected routes, the view could show one of several things on the same route.
-              Visiting localhost:3000/home will show the UserPage if the user is logged in.
-              If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-              Even though it seems like they are different pages, the user is always on localhost:3000/home */}
 
               <ProtectedRoute exact path="/home" component={WelcomeScreen} />
 
@@ -68,14 +59,7 @@ class App extends Component {
                 path="/organizationHome"
                 component={OrganizationHomeScreen}
               />
-              {/* This works the same as the other protected route, except that if the user is logged in,
-              they will see the info page instead. */}
-              <ProtectedRoute exact path="/info" component={InfoPage} />
-              <ProtectedRoute
-                exact
-                path="/devicePrep"
-                component={DevicePrep}
-              />
+
               <ProtectedRoute
                 exact
                 path="/createOrganization"
