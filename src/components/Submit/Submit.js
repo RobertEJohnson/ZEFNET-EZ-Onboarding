5b2b563db7d88aa22441cac17e04cfee5587e42e
Ace Fox <afox09@gmail.com>
2020-09-06 21:55:13 -0500

    hardcoded mockup for submit page

---
+++ src/components/Submit/Submit.js
@@ -0,0 +1,327 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {Grid, Paper, Button, withStyles, Divider, Accordion, TableHead, TableRow} from '@material-ui/core';
+import {AccordionSummary, AccordionDetails, Table, TableCell, TableBody} from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import SaveIcon from '@material-ui/icons/SaveAlt';
+import EditIcon from '@material-ui/icons/Edit';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const styles = theme => ({ 
+  root: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justify: 'center',
+    color: theme.palette.text.secondary,
+    fontFamily: 'Crimson Text, Open Sans, sans-serif',
+    minHeight: '80vh', 
+    background: 'white',
+    textAlign: 'center',
+  },
+  paper:{
+    padding: theme.spacing(2),
+    borderRadius: '5px',
+  },
+  gridListTile: {
+    color: 'rgba(255, 255, 255, 0.54)',
+    maxWidth: '300px',
+    maxHeight: '300px',
+    margin: '15px 15px 0px 0px'
+  },
+  image: {
+    maxHeight: '350px',
+    minWidth: '350px'
+  },
+  greyscale: {
+    filter: 'grayscale(100%)'
+  },
+  grow:{
+    flexGrow: 1,
+    minWidth: '100px'
+  },
+  reviewItem: {
+    display: 'inline-block',
+    padding: '0px',
+    margin: '0px'
+  },
+  table: {
+    minWidth: 500,
+  },
+})
+
+class Submit extends Component {
+
+    
+
+    render() {
+      const {classes} = this.props;
+      return (
+          <Grid container direction='row' justify='center' alignContent='center' alignItems='center' >
+          <div className = {classes.root} style={{maxWidth: '1000px'}}>
+            <Paper className = {classes.paper}>
+                <Grid item align='center'>
+                    <h1>All Devices and ZefNet Users Added?</h1>
+                    <h3>Let's review all your data before you submit.</h3>
+                    <Divider/>
+                    <h2>Here are your chosen users and corresponding information</h2>
+                    <h4>Please double-check all information and each user's privileges</h4>
+                    <Grid container direction = 'row-reverse'>
+                        <Button component = {Link} to ="/addUser">
+                            Edit <EditIcon/>
+                        </Button>
+                    </Grid>
+                    <Table className={classes.table}>
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>First Name</TableCell>
+                            <TableCell align="right">Last Name</TableCell>
+                            <TableCell align="right">Email Address</TableCell>
+                            <TableCell align="right">Phone Number</TableCell>
+                            <TableCell align="right">Priveleges</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody> 
+                            <TableRow >
+                            <TableCell component="th" scope="row">
+                                Ace
+                            </TableCell>
+                            <TableCell align="right">Fox</TableCell>
+                            <TableCell align="right">afox09@gmail.com</TableCell>
+                            <TableCell align="right">(203)731-1310</TableCell>
+                            <TableCell align="right">View</TableCell>
+                            </TableRow>
+                            <TableRow >
+                            <TableCell component="th" scope="row">
+                                Amir
+                            </TableCell>
+                            <TableCell align="right">Mussa</TableCell>
+                            <TableCell align="right">exampleEmail@email.com</TableCell>
+                            <TableCell align="right">(612) 701-3316</TableCell>
+                            <TableCell align="right">Edit</TableCell>
+                            </TableRow>
+                            <TableRow >
+                            <TableCell component="th" scope="row">
+                                Rob
+                            </TableCell>
+                            <TableCell align="right">Wolfe</TableCell>
+                            <TableCell align="right">exampleEmail@email.com</TableCell>
+                            <TableCell align="right">(612) 701-3316</TableCell>
+                            <TableCell align="right">Edit</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Grid>
+           <Divider/>
+            <Grid item align = 'center' xs = {12}>
+                <h2>Here are your devices, with breaker and location information</h2>
+                <h2>Please open each device to ensure information accuracy</h2>
+            </Grid>
+            <Accordion>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <h2>Device Name</h2>
+                    </AccordionSummary>
+                    
+                    <AccordionDetails>
+                        <Grid container direction='row' justify='center' alignContent='center' alignItems='center' >
+                                <Grid item align='center' xs={12}>
+                                    
+                                    <Divider/>
+                                    <Grid container direction = 'row' alignItems = 'center' justify = 'space-between'>
+                                        <div>
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                        </div>
+                                        <h2>Hosting Location</h2> 
+                                        <Button component = {Link} to ="/hostSelect">
+                                            Edit <EditIcon/>
+                                        </Button>
+                                </Grid>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <h3 style={{display: 'inline-block'}}>Address:{'\u00A0'}{'\u00A0'}</h3>
+                                    <p style={{display: 'inline-block'}}>
+                                        {this.props.state.device.site.address}
+                                    </p>
+                                </Grid>
+
+                                <Grid item align='center' xs={12}>
+                                    <br/>
+                                    <Divider/>
+                                <h2>Local Contact for Location</h2>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginTop: '10px'}}>First Name:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.site.first_name}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.reviewItem}>Second Name:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.site.second_name}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.reviewItem}>Phone Number:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.site.phone}
+                                        </p>
+                                    </div> 
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginBottom: '10px'}}>Email:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.site.email}
+                                        </p>
+                                    </div>
+                                </Grid>
+                            
+                                <Grid item align='center' xs={12}>
+                                    <Grid container direction = 'row' alignItems = 'center' justify = 'space-between'>
+                                        <div>
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                        </div>
+                                        <h2>Breaker Information</h2> 
+                                        <Button component = {Link} to ="/breakerSelect">
+                                            Edit <EditIcon/>
+                                        </Button>
+                                </Grid>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginTop: '10px'}}>Breaker Name:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.breaker.name}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.reviewItem}>Breaker Limit:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.breaker.limit}kW
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginBottom: '10px'}}>Breaker Description:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.breaker.description}
+                                        </p>
+                                    </div>
+                                </Grid>
+
+                                <Grid item align='center' xs={12}>
+                                    <Grid container direction = 'row' alignItems = 'center' justify = 'space-between'>
+                                        <div>
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                        </div>
+                                        <h2>Device Type</h2> 
+                                        <Button component = {Link} to ="/deviceType">
+                                            Edit <EditIcon/>
+                                        </Button>
+                                </Grid>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginTop: '10px'}}>Device Type:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                            {this.props.state.device.type.name}
+                                        </p>
+                                    </div>
+                                </Grid>
+
+                                <Grid item align='center' xs={12}>
+                                    <Grid container direction = 'row' alignItems = 'center' justify = 'space-between'>
+                                        <div>
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                        </div>
+                                        <h2>Device Information</h2> 
+                                        <Button component = {Link} to ="/deviceSerial">
+                                            Edit <EditIcon/>
+                                        </Button>
+                                </Grid>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{margin: '10px 0px'}}>Serial Number:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                        {this.props.state.device.serial.number}
+                                        </p>
+                                    </div>
+                                </Grid>
+
+                                <Grid item align='center' xs={12}>
+                                <Grid container direction = 'row' alignItems = 'center' justify = 'space-between'>
+                                        <div>
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                            {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
+                                        </div>
+                                        <h2>Additional Information</h2> 
+                                        <Button component = {Link} to ="/deviceName">
+                                            Edit <EditIcon/>
+                                        </Button>
+                                </Grid>
+                                </Grid>
+
+                                <Grid item align='center' xs={12} style={{backgroundColor: 'lightgrey'}}>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginTop: '10px'}}>Charger Name:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem}>
+                                        {this.props.state.device.name}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <h3 className={classes.reviewItem} style={{marginBottom: '10px'}}>Installation Date:{'\u00A0'}{'\u00A0'}</h3>
+                                        <p className={classes.reviewItem} >
+                                            {this.props.state.device.date}
+                                        </p>
+                                    </div>
+                                </Grid>
+                            
+                            </Grid>
+                        </AccordionDetails>
+                 </Accordion>
+                    
+                    <Grid container direction = 'row' justify = 'center' alignContent = 'center' style={{marginTop: '35px'}}>
+                            <Button variant ='contained'
+                            component = {Link} to ="/organizationHome">
+                                <ChevronLeftIcon/> Home
+                            </Button>
+                            <div className = {classes.grow}>{'\u00A0'}</div>
+                            <Button variant = 'contained' color = 'primary'
+                            size = 'large'
+                            >
+                                <SaveIcon/> Submit Onboarding Package for Review
+                            </Button>
+                        
+                        </Grid> 
+                    
+            </Paper>
+          </div>  
+          </Grid>
+      );
+    }
+  }
+  
+ Submit.propTypes = {
+      classes: PropTypes.object.isRequired,
+  };
+
+  const mapStateToProps = state => ({
+      state
+  })
+  
+  export default withStyles(styles)(connect(mapStateToProps)(Submit));
