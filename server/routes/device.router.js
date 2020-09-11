9c38debac5926e043cca6475cd873aa5d68d5d6b
Ace Fox <afox09@gmail.com>
2020-09-11 17:41:09 -0500

    updated sql and routes to accept second serial number

--- server/routes/device.router.js
+++ server/routes/device.router.js
@@ -10,7 +10,8 @@ router.get("/:id", rejectUnauthenticated, (req, res) => {
     const queryString = `
     SELECT "device"."name",  
 	"device"."install_date",
-	"device"."serial_number",
+  "device"."serial_number",
+  "device"."serial_number2",
 	"device"."id",
   "device"."breaker_id",
   "device"."type_id",
@@ -58,12 +59,13 @@ ORDER BY "device"."id" ASC;`;
  */
 router.post('/', rejectUnauthenticated, (req, res) => {
     const queryString = `INSERT INTO "device"
-    ("type_id", "breaker_id", "serial_number", "name", "install_date")
-    VALUES($1, $2, $3, $4, $5);`;
+    ("type_id", "breaker_id", "serial_number", "serial_number2", "name", "install_date")
+    VALUES($1, $2, $3, $4, $5, $6);`;
     const postValues = [
         req.body.type_id,
         req.body.breaker_id,
         req.body.serial_number,
+        req.body.serial_number2,
         req.body.name,
         req.body.installation_date
     ]
@@ -83,13 +85,15 @@ router.put('/:id', rejectUnauthenticated, (req, res) => {
   SET "type_id" = $1,
     "breaker_id" = $2,
     "serial_number" = $3,
-    "name" = $4,
-    "install_date" = $5
-  WHERE id = $6;`;
+    "serial_number2" = $4,
+    "name" = $5,
+    "install_date" = $6
+  WHERE id = $7;`;
   const postValues = [
       req.body.type_id,
       req.body.breaker_id,
       req.body.serial_number,
+      req.body.serial_number2,
       req.body.name,
       req.body.installation_date,
       req.params.id
